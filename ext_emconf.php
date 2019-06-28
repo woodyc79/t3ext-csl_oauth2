65aa60ca594abecf1f627a08aa1c5e58ea969263
Christian Holzmann <mail@innovations.at>
2019-06-28 12:23:53 +0200

    Fix PHP Version

--- ext_emconf.php
+++ ext_emconf.php
@@ -32,7 +32,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '0.1.0-dev',
     'constraints' => array(
         'depends' => array(
-            'php' => '5.5.0-7.0.99',
+            'php' => '5.5.0-7.2.99',
             'typo3' => '6.2.0-8.99.99',
             'saltedpasswords' => '',
         ),
